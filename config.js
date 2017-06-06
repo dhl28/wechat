8ac7c617d5cae19180af06886db21123c4918733
douhl <douhl@yonyou.com>
2017-06-06 20:38:10 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dhl on 2017/6/6.
+ */
+var config = {
+    token: 'hello2017',
+    appid: 'wxb907dfb2faa05934',
+    encodingAESKey: 'KdBVc9hKqNQZnroDR4cO2ojizAwATrilzxlwY3I2wYY'
+};
+module.exports = config;
