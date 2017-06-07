5ebeb07b9aeaae533ed261b2ea4d78d31e6870ca
dhl28 <dhl28@126.com>
2017-06-07 20:35:54 +0800

    dd

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
  */
 var config = {
     token: 'hello2017',
-    appid: 'wxb907dfb2faa05934',
+    // appid: 'wxb907dfb2faa05934',
+    appid: 'wxcaf2b638d25c2cc4', //测试号
     encodingAESKey: 'KdBVc9hKqNQZnroDR4cO2ojizAwATrilzxlwY3I2wYY'
 };
 module.exports = config;
