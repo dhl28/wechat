e993c75f35da61a467fc44d1446e391bdc94be0b
dhl28 <dhl28@126.com>
2017-06-08 11:02:58 +0800

    add node-cache

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var rp = require('request-promise');
+var cache = require('memory-cache');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -8,22 +9,28 @@ router.get('/', function (req, res, next) {
 });
 router.get('/accessToken', function (req, res) {
     var  uri = ' https://api.weixin.qq.com/sns/oauth2/access_token';
-    console.log('node server catch the token req');
-    console.log(req);
-    console.log('req info is logged');
-    rp({
-        uri: uri,
-        qs: req.query,
-        useQuerystring: true,
-        method: 'GET',
-        json: true
-    }).then(
-        function (body) {
-            res.json(body);
-        }
-    ).catch(function (err) {
-        handleError(res, err)
-    });
+    var code = req.query.code;
+    if(cache[code]){
+        console.log('cache value');
+        res.json(cache[code]);
+    }else {
+        rp({
+            uri: uri,
+            qs: req.query,
+            useQuerystring: true,
+            method: 'GET',
+            json: true
+        }).then(
+            function (body) {
+                console.log(JSON.parse(body));
+                cache.put(code.toString(),JSON.parse(body));
+                res.json(body);
+            }
+        ).catch(function (err) {
+            handleError(res, err)
+        });
+    }
+
 });
 
 module.exports = router;
