4743415f6fdc952ad11909fe6b3f4670ce976b9c
dhl28 <dhl28@126.com>
2017-06-08 23:08:52 +0800

    dd

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
     })
       $('#chooseImage').on('click',function (e) {
           wx.chooseImage({
-              count: 1, // 默认9
+              // count: 1, // 默认9
               sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
               sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
               success: function (res) {
