1a667d03c7c0d1c5781b816c16892920d1aa3bdf
dhl28 <dhl28@126.com>
2017-06-11 16:06:00 +0800

    'dd'

--- app.js
+++ app.js
@@ -34,31 +34,7 @@ app.use('/wechat',wechat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
     res.render('index');
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('index', {
-    message: err.message,
-    error: {}
-  });
 });
-
 
 module.exports = app;
