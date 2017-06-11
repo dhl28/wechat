77f56140a54d70eb7e0368d795cae5b78901cc03
dhl28 <dhl28@126.com>
2017-06-11 15:26:39 +0800

    dd

---
+++ public/javascripts/index.js
@@ -0,0 +1,44 @@
+/**
+ * Created by dhl on 2017/6/11.
+ */
+// 0. If using a module system, call Vue.use(VueRouter)
+
+// 1. Define route components.
+// These can be imported from other files
+const User = {template: '<user></user>'}
+const Bar = {template: '<div>bar</div>'}
+
+var data = {
+    msg:'this is user component'
+};
+
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+    {path: '/users', component: User},
+    {path: '/bar', component: Bar}
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+    router: router,
+    methods: {
+        linkTo: function (url) {
+            router.push(url)
+        }
+    }
+}).$mount('#app')
---
+++ public/javascripts/userComponent.js
@@ -0,0 +1,92 @@
+/**
+ * Created by dhl on 2017/6/11.
+ */
+var userModal ={
+
+}
+userModal.data = {
+    msg:'this is user component'
+}
+userModal.methods ={
+    viewUserInfo:function () {
+        $("#userInfoDetail").toggle();
+        console.log('click event trigger');
+    },
+    chooseImage:function () {
+        wx.chooseImage({
+            // count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                $('#localIds').html(JSON.stringify(res.localIds));
+            }
+        });
+    },
+}
+userModal.mounted= function () {
+    console.log(window.location.href);
+    var code = util.getQueryString('code');
+    var url = '/users/accessToken';
+    $.ajax({
+        url: url,
+        data: {
+            appid: constant.appid,
+            secret: constant.appsecret,
+            code: code,
+            grant_type: constant.AUTHORIZATION_CODE
+        }
+    }).done(function (d) {
+        console.log(d);
+        window.tokenMeta = d;//缓存token
+        $.ajax({
+            url: '/users/getUserInfo',
+            data: {
+                access_token: d.access_token,
+                openid: d.openid,
+                lang: 'zh_CN '
+            }
+        }).then(function (d) {
+            console.log('--------userInfo start--------');
+            console.log(d);
+            console.log('--------userInfo end--------');
+            $('#userInfoDetail').text(JSON.stringify(d));
+            $('#nickname').text(d.nickname);
+            $('#sex').text(d.sex == 1 ? '男' : '女');
+        })
+        //获取jsapiTicket
+        $.ajax({
+            url: '/users/jsapiTicket',
+            data: {
+                accessToken: d.access_token,
+                url: window.location.href
+            }
+        }).then(function (d) {
+            console.log(d);
+
+            wx.config({
+                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: constant.appid, // 必填，公众号的唯一标识
+                timestamp: d.timestamp, // 必填，生成签名的时间戳
+                nonceStr: d.nonceStr, // 必填，生成签名的随机串
+                signature: d.signature,// 必填，签名，见附录1
+                jsApiList: constant.API_LIST // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
+            wx.ready(function () {
+                console.log('wx jsapi is ready');
+            });
+        })
+
+    }).fail(function (err) {
+        console.log(err);
+    })
+};
+
+Vue.component('user', {
+    template: '#user-tpl',
+    data: function () {
+        return userModal.data;
+    },
+    methods:userModal.methods,
+    mounted:userModal.mounted
+})
