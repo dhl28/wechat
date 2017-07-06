71c6e7147f2f178d54e89fd775bac29b8040e1c4
dhl28 <dhl28@126.com>
2017-07-06 21:34:05 +0800

    add person homepage

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ module.exports = function (app) {
     app.use('/users',require('./users'))
     app.use('/register',require('./register'))
 
+    app.use('/fzzy', function(req, res, next) {
+        res.render('user/fzzy.html');
+    });
+
     app.use('/', function(req, res, next) {
         res.render('index.html');
     });
